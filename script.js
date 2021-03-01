e9d611ef0bfe00b733e7b7ccac87536c9d4163a2
Renato Magrini <renatomagrini@gmail.com>
2021-03-01 13:16:51 +0100

    score, currentscore and new game working

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ const score0 = document.querySelector(".score0");
 const score1 = document.querySelector(".score1");
 const current0 = document.querySelector(".current0");
 const current1 = document.querySelector(".current1");
-const newgame = document.querySelector("newgame");
+const newgame = document.querySelector(".newgame");
 const dice = document.querySelector(".dice");
 const roll = document.querySelector(".roll");
 const hold = document.querySelector(".hold");
 
+let currentScore, scores, activePlayer;
+
 const initial = () => {
+  currentScore = 0;
+  scores = [0, 0];
+  activePlayer = Math.floor(Math.random() * 2);
   score0.textContent = 0;
   score1.textContent = 0;
   current0.textContent = 0;
@@ -20,13 +25,23 @@ const initial = () => {
 };
 initial();
 
+// New Game
+newgame.addEventListener("click", initial);
+
 roll.addEventListener("click", function () {
   const random = Math.floor(Math.random() * 6 + 1);
   dice.src = `img/dice${random}.png`;
   if (random === 1) {
+    // SWITCH
     console.log("Switched");
   } else {
+    currentScore += random;
+    current0.textContent = currentScore;
   }
 });
 
-console.log();
+hold.addEventListener("click", function () {
+  scores[0] = currentScore;
+  score0.textContent = scores[0];
+  console.log(scores);
+});
